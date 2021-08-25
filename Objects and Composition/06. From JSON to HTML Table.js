e91ead32b48109bdd755a85bc1ae693c4c854456
darinchakalov <darinchakalov@hotmail.com>
2021-08-25 20:07:13 +0300

    Objects and Composition - Lab

---
+++ Objects and Composition/06. From JSON to HTML Table.js
@@ -0,0 +1,18 @@
+function solve(input) {
+    let arr = JSON.parse(input);
+    let outputArr = ["<table>"];
+     
+
+    function makeKeyRow(arr) {
+        
+    }
+    function makeValueRow(arr) {
+        
+    }
+    function escapeHtml(value) {
+        
+    }
+    console.log(outputArr.join('\n'));
+
+}
+solve('[{"Name":"Stamat", "Score":5.5}, {"Name":"Rumen", "Score":6}]');
