5e33736714038185b59bc3ee9129536cb724782f
darinchakalov <darinchakalov@hotmail.com>
2021-08-25 21:34:02 +0300

    1

---
+++ Objects and Composition/01. Calorie Object.js
@@ -0,0 +1,8 @@
+function solve(input) {
+    let result = {}
+    for (let i = 0; i < input.length; i+=2) {
+        const element = input[i];
+        
+    }
+}
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
